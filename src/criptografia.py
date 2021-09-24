64b56257cd452ee029887418f754b9a46c0e1d6b
Daniel de Souza Lima Junior <Dewaldman@outlook.com>
2021-09-24 16:35:08 -0300

    style: indentation by four

--- src/criptografia.py
+++ src/criptografia.py
@@ -5,57 +5,52 @@ from matplotlib import pyplot as plt
 import cv2
 
 
-
-#======================== INPUT IMAGE ========================
+# ======================== INPUT IMAGE ========================
 def input_img(image_name: str):
-  # Retorna as dimencoes da imagem e um objeto que é a sua imagem(img)
-  image = cv2.imread(image_name)
-  print("Dimensões da imagem: " + str(image.shape))
-  altura, largura, canais = image.shape
-  print(f'Largura: {largura}')
-  print(f'Altura: {altura}')
-  print(f'Canais de cores: {canais}')
-  return image, altura, largura, canais
+    # Retorna as dimencoes da imagem e um objeto que é a sua imagem(img)
+    image = cv2.imread(image_name)
+    print("Dimensões da imagem: " + str(image.shape))
+    altura, largura, canais = image.shape
+    print(f'Largura: {largura}')
+    print(f'Altura: {altura}')
+    print(f'Canais de cores: {canais}')
+    return image, altura, largura, canais
 
 
-
-#======================== SHOW IMAGE ========================
+# ======================== SHOW IMAGE ========================
 def show_image(image):
-  # Mostra Uma imagem
-  plt.figure(figsize = (15, 15))
-  plt.imshow(image)
-  plt.show()
+    # Mostra Uma imagem
+    plt.figure(figsize=(15, 15))
+    plt.imshow(image)
+    plt.show()
 
 
-
-#======================== IMAGE BUILDER ========================
+# ======================== IMAGE BUILDER ========================
 def image_builder(altura: int, largura: int, canais: int, intarray: list):
-  # Retorna uma imagem com o conteudo intarray
-  return np.ndarray(shape=(altura, largura, canais), dtype=int, buffer=np.array(list(intarray)))
+    # Retorna uma imagem com o conteudo intarray
+    return np.ndarray(shape=(altura, largura, canais), dtype=int, buffer=np.array(list(intarray)))
 
 
-
-
-#======================== CRIPTOGRAFAR IMAGEM ========================
+# ======================== CRIPTOGRAFAR IMAGEM ========================
 def criptografar(image, altura: int, largura: int, canais: int, operacao: str):
-  # ^ XOR
-  # & AND
-  # | OR
-  t = time()
-  op = {
-      'AND': lambda a, b : a & b,
-      'OR' : lambda a, b : a | b,
-      'XOR': lambda a, b : a ^ b
-  }
-  ENC = []
-  PWD = []
-  for y in range(0, altura):
-    for x in range(0, largura):
-      for z in range(canais):
-        mini_chave = randbelow(256) # 8 bits -> 2**8 = 256
-        PWD.append(mini_chave)
-        ENC.append(op[operacao](int(image[y][x][z]), mini_chave))
-  
-  print('Tempo de criptografia:', time() - t, 'sec')
-  
-  return ENC
+    # ^ XOR
+    # & AND
+    # | OR
+    t = time()
+    op = {
+        'AND': lambda a, b: a & b,
+        'OR': lambda a, b: a | b,
+        'XOR': lambda a, b: a ^ b
+    }
+    ENC = []
+    PWD = []
+    for y in range(0, altura):
+        for x in range(0, largura):
+            for z in range(canais):
+                mini_chave = randbelow(256)  # 8 bits -> 2**8 = 256
+                PWD.append(mini_chave)
+                ENC.append(op[operacao](int(image[y][x][z]), mini_chave))
+
+    print('Tempo de criptografia:', time() - t, 'sec')
+
+    return ENC
